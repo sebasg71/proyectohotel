bcabf7bcf5aab0790594df5f5f55dfb03fcf5a2d
Mateo Carvajal <mateocarva11@gmail.com>
2024-05-14 01:19:06 -0500

    modificacion en clase articulo ya que no se le habia puesto el tipo

---
+++ src/main/java/com/hotel/servicio/ArticuloServicio.java
@@ -0,0 +1,37 @@
+package com.hotel.servicio;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hotel.modelo.Articulo;
+import com.hotel.repositorio.ArticuloRepositorio;
+
+@Service
+public class ArticuloServicio implements ArticuloServicioImpl {
+
+    @Autowired
+    private ArticuloRepositorio articulorepositorio;
+
+    @Override
+    public List<Articulo> findAll() {
+        return (List<Articulo>) articulorepositorio.findAll();
+    }
+
+    @Override
+    public Articulo save(Articulo articulo) {
+
+        return articulorepositorio.save(articulo);
+    }
+
+    @Override
+    public Articulo buscarporId(Integer id) {
+        return articulorepositorio.findById(id).orElse(null);
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        articulorepositorio.deleteById(id);
+    }
+}
