227150d28d377f9f30fafc213e1e02821fc977ef
sebasg71 <141800371+sebasg71@users.noreply.github.com>
2024-05-16 00:23:49 -0500

    se actualiza la clase Articulos ,se le hace la relacion con habitacion

--- src/main/java/com/hotel/modelo/Articulo.java
+++ src/main/java/com/hotel/modelo/Articulo.java
@@ -5,66 +5,68 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "articulos")
+public class Articulo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_articulo")
+	private long idArticulo;
+	@Column(name = "nombre_articulo")
+	private String nombreArticulo;
+	@Column(name = "cantidad")
+	private int cantidad;
+	@Column(name = "descripcion")
+	private String descripcion;
+	@Column(name = "numero_habitacion")
+	private int numeroHabitacion;
 
-	@Entity
-	@Table(name="articulos")
-	public class Articulo {
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		@Column(name="id_articulo")
-		private long idArticulo;
-		@Column(name="nombre_articulo")
-		private String  nombreArticulo;
-		@Column(name="cantidad")
-		private int cantidad;
-		@Column(name="descripcion")
-		private String  descripcion;
-		@Column(name="numero_habitacion")
-		private int numeroHabitacion;
-		   
-			
-			public long getIdArticulo() {
-				return idArticulo;
-			}
+	@ManyToOne
+	@JoinColumn(name = "id_habitacion")
+	private Articulo articulo;
 
-			public void setIdArticulo(long idArticulo) {
-				this.idArticulo = idArticulo;
-			}
-
-			public String getNombreArticulo() {
-				return nombreArticulo;
-			}
-
-			public void setNombreArticulo(String nombreArticulo) {
-				this.nombreArticulo = nombreArticulo;
-			}
-
-			public int getCantidad() {
-				return cantidad;
-			}
-
-			public void setCantidad(int cantidad) {
-				this.cantidad = cantidad;
-			}
-
-			public String getDescripcion() {
-				return descripcion;
-			}
-
-			public void setDescripcion(String descripcion) {
-				this.descripcion = descripcion;
-			}
-
-			public int getNumeroHabitacion() {
-				return numeroHabitacion;
-			}
-
-			public void setNumeroHabitacion(int numeroHabitacion) {
-				this.numeroHabitacion = numeroHabitacion;
-			}
-
-			
-			
+	public long getIdArticulo() {
+		return idArticulo;
 	}
+
+	public void setIdArticulo(long idArticulo) {
+		this.idArticulo = idArticulo;
+	}
+
+	public String getNombreArticulo() {
+		return nombreArticulo;
+	}
+
+	public void setNombreArticulo(String nombreArticulo) {
+		this.nombreArticulo = nombreArticulo;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getNumeroHabitacion() {
+		return numeroHabitacion;
+	}
+
+	public void setNumeroHabitacion(int numeroHabitacion) {
+		this.numeroHabitacion = numeroHabitacion;
+	}
+
+}
