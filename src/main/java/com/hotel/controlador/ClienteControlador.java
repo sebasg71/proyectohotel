df11cd143bab698b15ce6704fed3a0a29da686c3
davidrami1205 <david.ramirez57@correo.tdea.edu.co>
2024-05-16 07:01:17 -0500

    Controlador Cliente, vista ListaCliente

--- src/main/java/com/hotel/controlador/ClienteControlador.java
+++ src/main/java/com/hotel/controlador/ClienteControlador.java
@@ -3,23 +3,46 @@ package com.hotel.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.hotel.modelo.Cliente;
 import com.hotel.servicio.ClienteServicio;
 
-@RestController
-@RequestMapping("/api/clientes")
+@Controller
+@RequestMapping("/vistas/Cliente")
 public class ClienteControlador {
 
     @Autowired
-    private ClienteServicio servicio;
+    private ClienteServicio clienteServicio;
 
-    @GetMapping
-    public List<Cliente> list(){
-        return servicio.findAll();
+    @GetMapping("/")
+    public String listadoClientes(Model modelo) {
+        List<Cliente> listadocliente = clienteServicio.findAll();
+        modelo.addAttribute("Cliente", listadocliente);
+        return "/vistas/Cliente/cliente";
+    }
+
+    @GetMapping("/create")
+    public String crear(Model modelo) {
+        // crear objeto
+        Cliente cliente = new Cliente();
+        modelo.addAttribute("Titulo", "Formulario Nuevo Cliente");
+        modelo.addAttribute("Cliente", cliente);
+        return "/vistas/Clientes/";
+    }
+
+    @PostMapping("/save")
+    public String guardar(@ModelAttribute Cliente cliente, Model modelo) {
+        modelo.addAttribute("Titulo: ", "Formulario: Nuevo Cliente");
+        modelo.addAttribute("Cliente", cliente);
+        clienteServicio.save(cliente);
+        return "redirect:/vistas/Cliente";
+
     }
 
 }
