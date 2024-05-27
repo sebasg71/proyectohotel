9c55dd7cc52b6f80d68544d5f4bf17236e58a33d
davidrami1205 <david.ramirez57@correo.tdea.edu.co>
2024-05-27 14:28:33 -0500

    .

--- src/main/java/com/hotel/repositorio/ServicioRepositorio.java
+++ src/main/java/com/hotel/repositorio/ServicioRepositorio.java
@@ -4,10 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.hotel.modelo.Servicio;
 
-<<<<<<< HEAD
 public interface ServicioRepositorio extends CrudRepository<Servicio, Long> {
-=======
-public interface ServicioRepositorio extends CrudRepository<Servicio, Long>{
->>>>>>> 9c123015686c3f34604e739afd65daaac901f9d5
 
 }
